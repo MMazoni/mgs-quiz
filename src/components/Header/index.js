071b5fda99437b8c463ac95ef6da7a1e65019d2c
Matheus Mazoni <mmazoni.andrade@gmail.com>
2021-01-30 08:49:41 -0300

    open graph

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,9 +6,21 @@ export default function Header() {
   return (
     <Head>
       <title>{db.title}</title>
-      <meta property="og:title" content={db.title} key="title" />
-      <meta property="og:image" content={db.bg} key="image" />
-      <meta property="og:description" content={db.description} key="description" />
+      <meta
+        property="og:title"
+        content="Quiz Metal Gear Solid"
+        key="title"
+      />
+      <meta
+        property="og:image"
+        content="https://wallpaperaccess.com/full/1292071.jpg"
+        key="image"
+      />
+      <meta
+        property="og:description"
+        content="Teste seu conhecimento sobre um dos melhores jogo da vida!"
+        key="description"
+      />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
     </Head>
